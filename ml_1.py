7479ff017ac77b31bbcf5b9ed3a515f7b319836e
tinah <tinawhuang95@gmail.com>
2018-03-09 18:58:03 -0500

    linear regression model

--- ml_1.py
+++ ml_1.py
@@ -1,8 +1,11 @@
 # linear regression model using finance data
 
 import pandas as pd 
-import quandl
-import math
+import quandl, math
+quandl.ApiConfig.api_key = "2_i9TyPLhXHXBuDs_zTM"
+import numpy as np 
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -20,4 +23,19 @@ forecast_out = int(math.ceil(0.01*len(df))) # predict 1% in the future
 df['label'] = df[forecast_col].shift(-forecast_out) # shift upwards 1% into future
 
 df.dropna(inplace=True)
-print(df.head())
+
+X = np.array(df.drop(['label'], 1)) # features
+y = np.array(df['label']) # labels
+
+# scaling may be faster and more accurate because want range of -1 to 1 but need to do \
+# so with all other values in the future as well
+X = preprocessing.scale(X)
+
+# shuffles up and outputs training data and testing data 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression() # linear regression classifier
+clf.fit(X_train, y_train) # fit is train
+accuracy = clf.score(X_test, y_test) # score is test 
+
+print(accuracy)
