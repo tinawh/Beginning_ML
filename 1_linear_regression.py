eddef6c2b1a1b62518e07c0f3bc6d1e6d7c6aefb
tinah <tinawhuang95@gmail.com>
2018-03-18 15:07:45 -0400

    changed filenames

--- 1_linear_regression.py
+++ 1_linear_regression.py
@@ -17,6 +17,7 @@ df = df[['Adj. Open', 'Adj. High','Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
+# only Adj. Close is directly related to price so if drop Adj. Close becomes very bad
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 # want to predict the Adj. Close of the future as label
@@ -43,14 +44,16 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 # easy to switch algorithms, check documentation for n_jobs to thread
 clf = LinearRegression(n_jobs=-1) # linear regression
 # clf = svm.SVR(kernel='poly') # SVR
-clf.fit(X_train, y_train) # fit is train
+clf.fit(X_train, y_train) # fit is for train
+
+# pickling to save
 with open('linearregression.pickle', 'wb') as f:
 	pickle.dump(clf, f)
 
 pickle_in = open('linearregression.pickle', 'rb')
 clf = pickle.load(pickle_in)
 
-accuracy = clf.score(X_test, y_test) # score is test 
+accuracy = clf.score(X_test, y_test) # score is for test 
 forecast_set = clf.predict(X_lately)
 print(forecast_set)
 # print(forecast_set, accuracy, forecast_out)
