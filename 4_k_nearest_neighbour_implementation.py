a29f1004445a9a15ea8fca9ac780a2155f41a5a1
tinah <tinawhuang95@gmail.com>
2018-04-01 10:01:04 -0400

    added fake data

--- 4_k_nearest_neighbour_implementation.py
+++ 4_k_nearest_neighbour_implementation.py
@@ -1,10 +1,9 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings 
-from matplotlib import style 
 from collections import Counter
-style.use('fivethirtyeight') 
+import random
+import pandas as pd
 
 # features that correspond to class of k, r
 dataset = { 'k':[[1, 2], [2, 3], [3, 1]], 'r':[[6, 5], [7, 7], [8, 6]] } 
